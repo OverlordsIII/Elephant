88e37f18a58d55886afb0c20d988e469ad42cda1
Ronak Kothari <rk.ronakjkothari@gmail.com>
2022-01-19 21:00:14 -0800

    Symmetrical Profile Picture
    Allowed further customization and made symmetrical possibility.

--- script.js
+++ script.js
@@ -3,8 +3,11 @@ function createRandomProfilePic(){
     const ctx = canvas.getContext('2d');
 
     const colorRange = ["#E74C3C", "#3498DB", "#1ABC9C", "#F1C40F"];
+    const pictureTypes = ["random", "symmetrical"]
     const quality = 100;
     const size = 5;
+
+    let picType = Math.floor(Math.random() * pictureTypes.length)
 
     canvas.width = quality * size;
     canvas.height = quality * size;
@@ -15,16 +18,48 @@ function createRandomProfilePic(){
     ctx.fill();
     ctx.fillStyle = colorRange[Math.floor(Math.random() * colorRange.length)]
 
-    for(let i = 0; i < size; i++){
-        for(let j = 0; j < size; j++){
-            if(Math.floor(Math.random() * 2) == 0){
-                ctx.fillRect(i * quality, j * quality, quality, quality);
-                ctx.fill();
+    if(picType == 0){
+        for(let i = 0; i < size; i++){
+            for(let j = 0; j < size; j++){
+                if(Math.floor(Math.random() * 2) == 0){
+                    ctx.fillRect(i * quality, j * quality, quality, quality);
+                    ctx.fill();
+                }
+            }
+        }
+    } else if(picType == 1){
+        if(size % 2 == 0){
+            for(let i = 0; i < size / 2; i++){
+                for(let j = 0; j < size; j++){
+                    if(Math.floor(Math.random() * 2) == 0){
+                        ctx.fillRect(i * quality, j * quality, quality, quality);
+                        ctx.fillRect((size - i - 1) * quality, j * quality, quality, quality);
+                        ctx.fill();
+                    }
+                }
+            }
+        } else {
+            for(let i = 0; i < Math.floor(size / 2); i++){
+                for(let j = 0; j < size; j++){
+                    if(Math.floor(Math.random() * 2) == 0){
+                        ctx.fillRect(i * quality, j * quality, quality, quality);
+                        ctx.fillRect((size - i - 1) * quality, j * quality, quality, quality);
+                        ctx.fill();
+                    }
+                }
+            }
+            for(let i = 0; i < size; i++){
+                if(Math.floor(Math.random() * 2) == 0){
+                    ctx.fillRect(Math.floor(size / 2) * quality, i * quality, quality, quality);
+                    ctx.fill();
+                }
             }
         }
     }
 
     ctx.closePath();
+
+    console.log(pictureTypes[picType]);
 
     canvas.toBlob(function(blob) {
         var newImg = document.createElement('img'),
