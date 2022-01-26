16a356f896b8b7b3ed6f67cc0631572ab6c96eea
Ronak Kothari <rk.ronakjkothari@gmail.com>
2022-01-26 14:01:52 -0800

    Create Deck Finished
    Ability to Create Decks and store them using local storage finished and working!

--- app/script.js
+++ app/script.js
@@ -9,7 +9,7 @@ function addDefinition(index){
     inputDistribution[index - 1]++;
 
     newInput.placeholder = "Definition " + inputDistribution[index - 1];
-    newInput.classList.add('dinput-' + index + "-" + inputDistribution[index - 1])
+    newInput.id = 'dinput-' + index + "-" + inputDistribution[index - 1];
 
     document.querySelector(".ddiv-" + index).insertBefore(newInput, document.querySelector(".dbtn-" + index));
 }
@@ -31,6 +31,7 @@ function createNewCard(){
     definitionBtn.setAttribute('onclick', 'addDefinition(' + cardIndex + ')');
     definitionDiv.classList.add("ddiv-" + cardIndex);
     definitionBtn.classList.add("dbtn-" + cardIndex);
+    termInput.classList.add("term-input")
 
     definitionDiv.appendChild(definitionBtn);
 
@@ -42,6 +43,37 @@ function createNewCard(){
     cardDiv.insertBefore(newDiv, document.querySelector("#new-card-btn"));
 
     addDefinition(cardIndex);
+}
+
+function saveChanges(){
+    const terms = document.querySelectorAll(".term-input");
+    const title = document.querySelector("#create-deck-name");
+    const description = document.querySelector("#create-deck-desc");
+    const img = document.querySelector("#create-deck-img");
+
+    let newObject = {
+        desc: description.value,
+        image: img.value,
+        cards:0
+    }
+
+    let termsObj = {}
+    let tempList = [];
+
+    for(let i = 0; i < terms.length; i++){
+        for(let j = 0; j < inputDistribution[i]; j++){
+            tempList.push(document.getElementById("dinput-" + (i + 1) + "-" + (j+1)).value);
+        }
+        termsObj[terms[i].value] = tempList;
+
+        tempList = [];
+    }
+
+    newObject.cards = termsObj;
+    localStorage.setItem(title.value, newObject);
+
+    document.getElementById('create-modal').classList.add('inactive-modal');
+    document.getElementById('create-modal').classList.remove('active-modal')
 }
 
 createNewCard();
