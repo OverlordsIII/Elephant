cfebb8fab8d85decf3b3c64f8e7ec8b741f8bf67
Ronak Kothari <rk.ronakjkothari@gmail.com>
2022-02-24 10:56:12 -0800

    Working Flashcards
    Flashcards are now working!

--- app/review.js
+++ app/review.js
@@ -12,13 +12,6 @@ function setupReview(index){
 
     reviewDeck.cards = object.cards;
 
-    let child = document.getElementById('flashcard-answers').lastElementChild;
-
-    while (child) {
-        document.getElementById('flashcard-answers').removeChild(child);
-        child = document.getElementById('flashcard-answers').lastElementChild;
-    }
-
     for(let i = 0; i < object.cards.length; i++){
         reviewCards.push(false);
     }
@@ -26,12 +19,67 @@ function setupReview(index){
     setupAnswers();
 }
 
+function checkAnswer(index){
+
+    let answerDiv = document.createElement('div');
+    const parentElem = document.querySelectorAll('.flashcard-def-btn')[index];
+
+    if(correctAnswers.includes(index)){
+        correctAnswers[correctAnswers.indexOf(index)] = true;
+        answerDiv.classList.add('right');
+    } else {
+        answerDiv.classList.add('wrong');
+    }
+
+    let complete = true;
+    let i = 0;
+
+    while(complete){
+        if(correctAnswers[i] != true) {
+            complete = false;
+            break;
+        }
+        else if(i + 1 == correctAnswers.length) break;
+        i++
+    }
+
+    if(complete){
+        setupAnswers();
+    }
+
+    parentElem.appendChild(answerDiv);
+}
+
+function skipQuestion(index){
+    progress--;
+    setupAnswers();
+    reviewCards[index] = false;
+}
+
 function setupAnswers(){
     let randomCard = undefined;
     progress++
 
+    let child = document.getElementById('flashcard-answers').lastElementChild;
+
+    while (child) {
+        document.getElementById('flashcard-answers').removeChild(child);
+        child = document.getElementById('flashcard-answers').lastElementChild;
+    }
+
+    let complete = true;
+
+    for(let i = 0; i < reviewCards.length; i++){
+        if(reviewCards[i] != true){
+            complete = false;
+            break;
+        }
+    }
+
+    if(complete) return;
+
     const progressBar = document.getElementById('review-progress');
-    progressBar.innerHTML = "1/" + reviewCards.length + " Flashcards";
+    progressBar.innerHTML = progress + "/" + reviewCards.length + " Flashcards";
     progressBar.style.background = "linear-gradient(135deg, var(--theme-color-1) 0%, var(--theme-color-2) " + 100 * progress / reviewCards.length + "%, var(--bg-color-1) " + 100 * progress / reviewCards.length + "%)";
 
     while(randomCard == undefined){
@@ -39,9 +87,11 @@ function setupAnswers(){
         if(reviewCards[randomCard] == true) randomCard = undefined;
     }
 
+    reviewCards[randomCard] = true;
+
     let questionsRandomized = reviewDeck.multipleChoice(randomCard)
     let definitionsArray = questionsRandomized[0];
-    let correctAnswers = questionsRandomized[1];
+    correctAnswers = questionsRandomized[1];
 
     document.getElementById('flashcard-header-text').innerHTML = reviewDeck.cards[randomCard][0];
 
@@ -49,20 +99,17 @@ function setupAnswers(){
         let newDiv = document.createElement('div');
         let newBtn = document.createElement('button');
 
+        newBtn.setAttribute('onclick', "checkAnswer(" + i + ")")
         newBtn.innerHTML = definitionsArray[i];
-        if(correctAnswers.includes(i)){
-            let correct = document.createElement('div');
-            correct.classList.add('right');
-            newDiv.appendChild(correct);
-        }
-
         newDiv.appendChild(newBtn);
+        newDiv.classList.add('flashcard-def-btn')
         document.getElementById('flashcard-answers').appendChild(newDiv);
     }
 
     let skipBtn = document.createElement('button');
     skipBtn.innerHTML = "Skip Question";
     skipBtn.id = "skip-question-btn";
+    skipBtn.setAttribute('onclick', "skipQuestion(" + randomCard + ")");
 
     document.getElementById('flashcard-answers').appendChild(skipBtn);
 }
